81de82b7934c28c5c124ec3fcc640351412abe59
guapusing <rendisukirman.rs@gmail.com>
2017-04-03 13:30:41 +0700

    Initial commit

---
+++ app/src/main/java/com/example/king/loginrememixxing/Kucing.java
@@ -0,0 +1,58 @@
+package com.example.king.loginrememixxing;
+
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.app.NotificationCompat;
+import android.view.View;
+
+/**
+ * Created by king on 3/15/2017.
+ */
+public class Kucing extends AppCompatActivity {
+    // identititas id unik untuk notification
+    public static final int NOTIFICATION_ID = 1;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.kucing);
+
+
+        getSupportActionBar().setSubtitle("Belajar di okedroid.com");
+
+    }
+
+    public void tampilNotification(View view) {
+
+        Intent intent = new Intent(Intent.ACTION_VIEW,
+                Uri.parse("http://www.okedroid.com/"));
+        //menginisialiasasi intent
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, 0);
+        //untuk memanggil activity di Notification
+        /*
+Menmbangun atau mensetup Notification dengan NotificationCompat.Builder
+ */
+        NotificationCompat.Builder builder = (NotificationCompat.Builder) new NotificationCompat.Builder(this)
+                .setSmallIcon(R.mipmap.ic_launcher) //ikon notification
+                .setContentTitle("1 Postingan Baru")//judul konten
+                .setContentIntent(pendingIntent)//memanggil object pending intent
+                .setAutoCancel(true)//untuk menswipe atau menghapus notification
+                .setContentText("Kunjungi blog okedroid.com"); //isi text
+
+/*
+Kemudian kita harus menambahkan Notification dengan menggunakan NotificationManager
+ */
+
+        NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+
+        notificationManager.notify(NOTIFICATION_ID, builder.build()
+        );
+    }
+
+
+
+}
